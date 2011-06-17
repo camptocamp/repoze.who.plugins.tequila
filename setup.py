1621c9ee3f3ecdd9838447590b12574e348b992a
Frederic Junod <frederic.junod@camptocamp.com>
2011-06-17 11:24:40 +0200

    set package description

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='repoze.who.plugins.tequila',
       author='Frederic Junod',
       author_email='frederic.junod@camptocamp.com',
       url='https://github.com/camptocamp/repoze.who.plugins.tequila',
+      description='A repoze.who challenger for http://tequila.epfl.ch/',
       packages=find_packages(),
       zip_safe=False,
       install_requires=[
